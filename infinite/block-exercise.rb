2b6a06b9f4821a935f835734db2aa3cd9771a7a3
Shayne <shaynetremblay@hotmail.com>
2015-05-17 17:09:07 -0400

    hello_app

---
+++ infinite/block-exercise.rb
@@ -0,0 +1,36 @@
+# 1. Write method repeatit, which performs/executes the block passed to it twice
+def repeatit
+	2.times do
+		yield
+	end
+end
+
+repeatit do
+	puts 'I am repeating'
+end
+
+x = 1
+repeatit do
+	x = x + 1
+end
+puts x
+
+puts '*********'
+
+# 2. Array method "find" already exists: it returns the first occurence of an
+#    element satisfying its condition. Write the method "myfind", as if the
+#    built-in method didn't already exists
+
+class Array
+	def myfind
+		self.each do |val|
+			if yield(val)
+				return val
+			end
+		end
+	end
+end
+
+puts [9, 10, 11, 12, 13, 14, 15, 16].myfind{|x| x > 10} #should print 11
+
+puts [9, 10, 11, 12, 13, 14, 15, 16].myfind{|x| x % 6 == 0} #should print 12
